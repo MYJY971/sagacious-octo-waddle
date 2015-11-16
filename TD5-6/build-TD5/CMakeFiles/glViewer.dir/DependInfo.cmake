94cef4c04fe147167635e32991ea789751ae91ba
MYJY971 <yanis.mounsamy@gmail.com>
2015-11-16 14:29:25 +0100

    TD5

---
+++ TD5-6/build-TD5/CMakeFiles/glViewer.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/myjy/work/modelGeo/sagacious-octo-waddle/TD5-6/TD5/glfw/deps/glad.c" "/home/myjy/work/modelGeo/sagacious-octo-waddle/TD5-6/build-TD5/CMakeFiles/glViewer.dir/glfw/deps/glad.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/myjy/work/modelGeo/sagacious-octo-waddle/TD5-6/TD5/src/Mesh.cpp" "/home/myjy/work/modelGeo/sagacious-octo-waddle/TD5-6/build-TD5/CMakeFiles/glViewer.dir/src/Mesh.cpp.o"
+  "/home/myjy/work/modelGeo/sagacious-octo-waddle/TD5-6/TD5/src/Meshloader.cpp" "/home/myjy/work/modelGeo/sagacious-octo-waddle/TD5-6/build-TD5/CMakeFiles/glViewer.dir/src/Meshloader.cpp.o"
+  "/home/myjy/work/modelGeo/sagacious-octo-waddle/TD5-6/TD5/src/Octree.cpp" "/home/myjy/work/modelGeo/sagacious-octo-waddle/TD5-6/build-TD5/CMakeFiles/glViewer.dir/src/Octree.cpp.o"
+  "/home/myjy/work/modelGeo/sagacious-octo-waddle/TD5-6/TD5/src/Pointcloud.cpp" "/home/myjy/work/modelGeo/sagacious-octo-waddle/TD5-6/build-TD5/CMakeFiles/glViewer.dir/src/Pointcloud.cpp.o"
+  "/home/myjy/work/modelGeo/sagacious-octo-waddle/TD5-6/TD5/src/Shader.cpp" "/home/myjy/work/modelGeo/sagacious-octo-waddle/TD5-6/build-TD5/CMakeFiles/glViewer.dir/src/Shader.cpp.o"
+  "/home/myjy/work/modelGeo/sagacious-octo-waddle/TD5-6/TD5/src/Trackball.cpp" "/home/myjy/work/modelGeo/sagacious-octo-waddle/TD5-6/build-TD5/CMakeFiles/glViewer.dir/src/Trackball.cpp.o"
+  "/home/myjy/work/modelGeo/sagacious-octo-waddle/TD5-6/TD5/src/WireCube.cpp" "/home/myjy/work/modelGeo/sagacious-octo-waddle/TD5-6/build-TD5/CMakeFiles/glViewer.dir/src/WireCube.cpp.o"
+  "/home/myjy/work/modelGeo/sagacious-octo-waddle/TD5-6/TD5/src/main.cpp" "/home/myjy/work/modelGeo/sagacious-octo-waddle/TD5-6/build-TD5/CMakeFiles/glViewer.dir/src/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "PGHP_DIR=\"/home/myjy/work/modelGeo/sagacious-octo-waddle/TD5-6/TD5\""
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/myjy/work/modelGeo/sagacious-octo-waddle/TD5-6/build-TD5/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "/home/myjy/work/modelGeo/sagacious-octo-waddle/TD5-6/build-TD5/SOIL/CMakeFiles/SOIL.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/myjy/work/modelGeo/sagacious-octo-waddle/TD5-6/TD5/glfw/include"
+  "/home/myjy/work/modelGeo/sagacious-octo-waddle/TD5-6/TD5/glfw/deps"
+  "/home/myjy/work/modelGeo/sagacious-octo-waddle/TD5-6/TD5/SOIL/src"
+  "/home/myjy/work/modelGeo/sagacious-octo-waddle/TD5-6/TD5/include"
+  "/home/myjy/work/modelGeo/sagacious-octo-waddle/TD5-6/TD5/src"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ TD5-6/build-TD5/SOIL/CMakeFiles/SOIL.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/SOIL.dir/src/SOIL2.c.o"
+  "CMakeFiles/SOIL.dir/src/image_DXT.c.o"
+  "CMakeFiles/SOIL.dir/src/etc1_utils.c.o"
+  "CMakeFiles/SOIL.dir/src/image_helper.c.o"
+  "CMakeFiles/SOIL.dir/src/stb_image_write.c.o"
+  "CMakeFiles/SOIL.dir/src/stb_image.c.o"
+  "libSOIL.pdb"
+  "libSOIL.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/SOIL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
